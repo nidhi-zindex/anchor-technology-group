e99673c61eedbdb0da253112c4aea0c0641765b1
Nidhi Z Index <nidhinirmal.zindex@gmail.com>
2025-09-04 12:36:41 +0530

    commiting

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    // scroll to section
+    $.scrollIt({
+        upKey: 38,             // key code to navigate to the next section
+        downKey: 40,           // key code to navigate to the previous section
+        easing: 'linear',      // the easing function for animation
+        scrollTime: 600,       // how long (in ms) the animation takes
+        activeClass: 'active', // class given to the active nav element
+        onPageChange: null,    // function(pageIndex) that is called when page is changed
+        topOffset: -78           // offste (in px) for fixed top navigation
+    });
+
+});
